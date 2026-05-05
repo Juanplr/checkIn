1209494f7aa5b40e2992781a1d5ef1d480a74285
Soka_jplr <juanlazarorp@gmail.com>
2026-05-04 23:28:18 -0600

    add css and icons in header

--- check-proyect/src/components/header.tsx
+++ check-proyect/src/components/header.tsx
@@ -1,10 +1,29 @@
+import userIcon from '../assets/icons/user.svg'
+import ajustesIcon from '../assets/icons/setings.svg'
+import inventarioIcon from '../assets/icons/inventory.svg'
+import usuariosIcon from '../assets/icons/users.svg'
+import barCodeIcon from '../assets/icons/barcorder.svg'
+
 function Header() {
     return (
         <>
             <header>
-                <div><h1>Check-In</h1></div>
-                <div></div>
-                <div><img src="" alt="" /></div>
+                <div className='container header '>
+                    <div className='row align-items-center'>
+                        <div className='col d-flex justify-content-start'>
+                            <h1>Check-In</h1>
+                        </div>
+                        <div className='iconos col d-flex justify-content-between'>
+                            <img className='button-icon' src={ajustesIcon} alt="Ajustes" />
+                            <img className='button-icon' src={inventarioIcon} alt="Inventario" />
+                            <img className='button-icon' src={usuariosIcon} alt="Usuarios" />
+                            <img className='button-icon' src={barCodeIcon} alt="Código de Barras" />
+                        </div>
+                        <div className='usuario col d-flex justify-content-end'>
+                            <img className="button-icon" src={userIcon} alt="Usuario" />
+                        </div>
+                    </div>
+                </div>
             </header>
         </>
     )
--- check-proyect/src/pages/principal.tsx
+++ check-proyect/src/pages/principal.tsx
@@ -1,17 +1,16 @@
 import Header from "../components/header";
-
+import codigoBarraIcon from '../assets/icons/barcorder.svg'
 function Principal() {
   return (
     <>
       <Header />
-      <div>
+      <div className="container justify-content-center align-items-center d-flex flex-column">
         <div>
           <h2>Escanea el producto</h2>
         </div>
         <div>
-          <label htmlFor="018263"></label>
-          <button></button>
-          <img src="" alt="" />
+          <label>Codigo de Barra</label>
+          <img className="button-icon" src={codigoBarraIcon} alt="Código de barras" />
         </div>
         <div>
           <a href="">IniciarSession <img src="" alt="" /></a>
