37cd3ac79096858f6841a30efb5d9678bdba9ce8
Soka_jplr <juanlazarorp@gmail.com>
2026-05-05 23:17:00 -0600

    add barcode scaner

---
+++ check-proyect/src/components/barCode.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import BarcodeScanner from "react-qr-barcode-scanner";
+
+
+function BarCode() {
+    const [data, setData] = useState("Not Found");
+
+    return (
+        <>
+            <BarcodeScanner
+                width={500}
+                height={500}
+                onUpdate={(err, result) => {
+                    if (result) setData(result.getText());
+                    else setData("Not Found");
+                }}
+            />
+            <p>{data}</p>
+        </>
+    );
+}
+
+export default BarCode;
